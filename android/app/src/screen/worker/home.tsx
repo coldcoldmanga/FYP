7f96afeba7b35d79d4fa182264acb394ab4ac45f
Melvin <melvinkwanys@gmail.com>
2025-02-27 21:58:05 +0800

    included logout

--- android/app/src/screen/worker/home.tsx
+++ android/app/src/screen/worker/home.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { logout } from '../../services/authServices';
+import { NavigationProp } from '@react-navigation/native';
 
-const WorkerHome = () => {
+const WorkerHome = ({navigation}: {navigation: NavigationProp<any>}) => {
+    const handleLogout = async () => {
+        try{
+            await logout();
+            navigation.reset({
+                index: 0,
+                routes: [{name: 'SignUp'}]
+            })
+        }catch(error){
+            console.error('Logout Error: ', error);
+        }
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Worker Home</Text>
+            <Text>Maintenance Worker Home</Text>
+
+            <TouchableOpacity onPress={() => handleLogout()}>
+                <Text style={styles.linkText}>Log Out</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -16,5 +34,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    linkText: {
+        color: '#1a2847',
+        marginTop: 10,
+        textDecorationLine: 'underline',
+      },
 });
